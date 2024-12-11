ca966736fc584edb9184ba6a9f6fdabd862716fc
Kyoungwoong <kyy980708@gmail.com>
2024-12-12 01:24:17 +0900

    initialize

---
+++ src/main/java/com/streetfoodmap/streetfood_map/StreetfoodMapApplication.java
@@ -0,0 +1,13 @@
+package com.streetfoodmap.streetfood_map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreetfoodMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreetfoodMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/streetfoodmap/streetfood_map/StreetfoodMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streetfoodmap.streetfood_map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreetfoodMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
